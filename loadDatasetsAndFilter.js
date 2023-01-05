c4bc604bb9b51e81aee0ebdf84a8a94d76179f78
Miguel Ceriani <miguel.ceriani@gmail.com>
2023-01-06 00:40:15 +0100

    Read from local sources and fix RDF problems

--- loadDatasetsAndFilter.js
+++ loadDatasetsAndFilter.js
@@ -3,7 +3,6 @@ import https from 'node:https';
 import zlib from 'node:zlib';
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
-import fetch from 'node-fetch';
 import bz2 from 'unbzip2-stream';
 import N3 from 'n3';
 import N3TollerantStreamParser from './N3TollerantStreamParser.js';
@@ -18,6 +17,7 @@ const PROV_PREFIX = 'http://www.w3.org/ns/prov#';
 const LSQV_PREFIX = 'http://lsq.aksw.org/vocab#';
 
 const REMOTE_EXECUTION_PREFIX = 'http://lsq.aksw.org/re-';
+const QUERY_PREFIX = 'http://lsq.aksw.org/lsqQuery-';
 
 const EXECUTION_IRI_PROPERTY_NAME_MAPPING = {
     [PROV_PREFIX + 'atTime']: 'atTime',
@@ -57,6 +57,10 @@ class ExecutionExtractor extends Transform {
     executionCache = {};
     executionToQueryCache = {};
 
+    queryCache = {};
+    confirmedQueries = new Set();
+    queriesToBeIncluded = new Set();
+
     constructor() {
         super({objectMode: true});
     }
@@ -66,12 +70,55 @@ class ExecutionExtractor extends Transform {
             this.addExecutionProperty(quad._subject.id, EXECUTION_IRI_PROPERTY_NAME_MAPPING[quad._predicate.id], quad._object);
         } else if (quad._predicate.id === LSQV_PREFIX + 'hasRemoteExec') {
             this.addQueryExecutionAssociation(quad._subject.id, quad._object.id);
+            this.includeQuery(quad._subject.id);
+        } else if (quad._predicate.id === LSQV_PREFIX + 'text') {
+            this.addQuery(quad._subject.id, quad._object);
         }
         callback();
     }
 
+    getQueryHash(queryId) {
+        return queryId.substring(QUERY_PREFIX.length);
+    }
+
+    addQuery(queryId, queryText) {
+        const queryHash = this.getQueryHash(queryId);
+        if (this.confirmedQueries.has(queryHash)) {
+            return;
+        }
+        if (this.queriesToBeIncluded.has(queryHash)) {
+            this.confirmedQueries.add(queryHash);
+            this.outputQueryInfo(queryId, queryHash, queryText);
+            this.queriesToBeIncluded.delete(queryHash);
+        } else {
+            this.queryCache[queryHash] = queryText;
+        }
+    }
+
+    includeQuery(queryId) {
+        const queryHash = this.getQueryHash(queryId);
+        if (this.confirmedQueries.has(queryHash)) {
+            return;
+        }
+        if (queryHash in this.queryCache) {
+            this.confirmedQueries.add(queryHash);
+            this.outputQueryInfo(queryId, queryHash, this.queryCache[queryHash]);
+            delete this.queryCache[queryHash];
+        } else { 
+            this.queriesToBeIncluded.add(queryHash);
+        }
+    }
+
+    outputQueryInfo(queryId, queryHash, queryText) {
+        this.push({
+            type: 'query',
+            queryId, queryHash, queryText
+        });
+    }
+
     outputExecutionInfo(queryId, executionId, executionInfo) {
         this.push({
+            type: 'execution',
             queryId, executionId, executionInfo
         });
     }
@@ -83,9 +130,6 @@ class ExecutionExtractor extends Transform {
     }
 
     addQueryExecutionAssociation(queryId, executionId) {
-        // console.log('addQueryExecutionAssociation');
-        // console.log(`queryId: ${queryId}, executionId: ${executionId}`);
-        // console.log(`currentExecution: ${JSON.stringify(this.currentExecution, null, 2)}`);
         if (executionId in this.executionCache) {
             const executionInfoArray = this.executionCache[executionId].shift();
             if (this.executionCache[executionId].length === 0) {
@@ -152,29 +196,6 @@ const JSONLD_BEGIN = `{"@context":${JSON.stringify({
 })},"@graph":[`;
 const JSONLD_END = ']}';
 
-class ExecutionAsJsonLdSerializer extends Transform {
-    execCount = 0;
-    constructor() {
-        super({
-            writableObjectMode: true,
-            transform({queryId, executionInfo}, encoding, callback) {
-                const executionId = ''; // TODO generate IRI
-                this.push((this.execCount ? JSONLD_BEGIN : ',') + JSON.stringify({
-                    '@id': executionId,
-                    'isRemoteExecOf': queryId,
-                    ...executionInfo
-                }));
-                this.first = false;
-                callback();
-            },
-            flush(callback) {
-                this.push(this.first ? '{}' : JSONLD_END);
-                callback();
-            }
-        });
-    }    
-}
-
 class ExecutionAsQuadsSerializer extends Transform {
     execCount = 0;
     constructor(dataset) {
@@ -201,6 +222,29 @@ class ExecutionAsQuadsSerializer extends Transform {
     }
 }
 
+class QueryAsQuadsSerializer extends Transform {
+    execCount = 0;
+    constructor(dataset) {
+        super({objectMode: true});
+        this.dataset = dataset;
+    }
+
+    _transform({queryId, queryHash, queryText}, encoding, callback) {
+        // const queryId = QUERY_PREFIX + queryHash;
+        this.push(quad(
+            namedNode(queryId),
+            namedNode(LSQV_PREFIX + 'hash'),
+            literal(queryHash)
+        ));
+        this.push(quad(
+            namedNode(queryId),
+            namedNode(LSQV_PREFIX + 'text'),
+            queryText
+        ));
+        callback();
+    }
+}
+
 class ObjectLogger extends Transform {
     constructor(dataset) {
         super({objectMode: true});
@@ -212,26 +256,78 @@ class ObjectLogger extends Transform {
 };
 
 
-async function loadDatasetAndFilter(dataset, datasetId, options) {
-    const {remoteDirPath, tmpDirPath, outputDirPath} = options;
-    const remoteFilePath = remoteDirPath + datasetId + '.nt.bz2';
-    const response = await fetch(remoteFilePath);
-    if (!response.ok) {
-        throw new Error(response.statusText);
+class DetourStream extends Transform {
+    constructor(filterFn, stream) {
+        super({objectMode: true});
+        this.filterFn = filterFn;
+        this.stream = stream;
     }
+    _transform(data, encoding, callback) {
+        if (this.filterFn(data)) {
+            this.stream.write(data);
+            callback();
+        } else {
+            callback(null, data);
+        }
+    }
+    _flush(callback) {
+        this.stream.end();
+        callback();
+    }
+};
 
-    const outputFilePath = outputDirPath + datasetId + '.nt.gz';
+async function loadDatasetAndFilter(dataset, datasetId, options) {
+    const {inputDirPath, outputDirPath, outputCsv} = options;
+    // const remoteFilePath = remoteDirPath + datasetId + '.nt.bz2';
+    // const response = await fetch(remoteFilePath);
+    // if (!response.ok) {
+    //     throw new Error(response.statusText);
+    // }
 
-    await pipeline(
-        response.body,
-        bz2(),
-        new N3TollerantStreamParser({format: 'N-Triples'}),
-        new ExecutionExtractor(),
-        new ExecutionAsQuadsSerializer(dataset),
-        new N3.StreamWriter({format: 'N-Triples'}),
-        zlib.createGzip(),
-        fs.createWriteStream(outputFilePath)
-    );
+    const inputFilePath = inputDirPath + datasetId + '.nt.bz2';
+
+    const executionExtractor = new ExecutionExtractor();
+
+    if (outputCsv) {
+        const outputFilePath = `${outputDirPath}${datasetId}.csv.gz`;
+
+        await pipeline(
+            // response.body,
+            fs.createReadStream(inputFilePath),
+            bz2(),
+            new N3TollerantStreamParser({format: 'N-Triples'}),
+            new ExecutionExtractor(),
+            new ExecutionAsQuadsSerializer(dataset), // TODO: change
+            new N3.StreamWriter({format: 'N-Triples'}), // TODO: change
+            zlib.createGzip(),
+            fs.createWriteStream(outputFilePath)
+        );
+    } else {
+        // const outputFilePath = `${outputDirPath}${datasetId}_execs.nt.gz`;
+
+        function ntGzWriter(filePath) {
+            const writer = new N3.StreamWriter({format: 'N-Triples'});
+            writer.pipe(zlib.createGzip()).pipe(fs.createWriteStream(filePath));
+            return writer;
+        }
+
+        const querySink = new QueryAsQuadsSerializer(dataset);
+        querySink.pipe(ntGzWriter(`${outputDirPath}${datasetId}_queries.nt.gz`));
+
+        const executionSink = new ExecutionAsQuadsSerializer(dataset);
+        executionSink.pipe(ntGzWriter(`${outputDirPath}${datasetId}_execs.nt.gz`));
+
+        await pipeline(
+            // response.body,
+            fs.createReadStream(inputFilePath),
+            bz2(),
+            new N3TollerantStreamParser({format: 'N-Triples'}),
+            executionExtractor,
+            new DetourStream(({type}) => (type === 'query'), querySink),
+            executionSink
+        );
+
+    }
 }
 
 // async function* asyncMap(input, fn) {
@@ -282,8 +378,10 @@ async function main() {
         inputEndpointURL: 'http://localhost:3030/lsq2/query', 
         datasetsGraphname: 'http://lsq.aksw.org/datasets',
         remoteDirPath: 'https://hobbitdata.informatik.uni-leipzig.de/lsqv2/dumps/',
-        tmpDirPath: './tmp/rdf/datasets/',
+        // outputDirPath: './input/datasets/csv/',
+        inputDirPath: './input/lsq2-source/',
         outputDirPath: './input/rdf/datasets/',
+        // outputCsv: true,
         excludeDatasets: [
             'http://lsq.aksw.org/datasets/bench-affymetrix-lsq2',
             'http://lsq.aksw.org/datasets/bench-biomedels-lsq2',
@@ -299,31 +397,31 @@ async function main() {
             'http://lsq.aksw.org/datasets/bench-dbpedia-20160222-lsq2',
             'http://lsq.aksw.org/datasets/bench-dbpedia-20160301-lsq2',
             'http://lsq.aksw.org/datasets/bench-dbpedia-20160303-lsq2',
-            'http://lsq.aksw.org/datasets/bench-dbpedia-20160304-lsq2',
-            'http://lsq.aksw.org/datasets/bench-dbpedia-20160314-lsq2',
-            'http://lsq.aksw.org/datasets/bench-dbpedia-20160411-lsq2',
-            'http://lsq.aksw.org/datasets/bench-dbpedia.3.5.1.log-lsq2',
+            // 'http://lsq.aksw.org/datasets/bench-dbpedia-20160304-lsq2',
+            // 'http://lsq.aksw.org/datasets/bench-dbpedia-20160314-lsq2',
+            // 'http://lsq.aksw.org/datasets/bench-dbpedia-20160411-lsq2',
+            // 'http://lsq.aksw.org/datasets/bench-dbpedia.3.5.1.log-lsq2',
             'http://lsq.aksw.org/datasets/bench-dbsnp-lsq2',
             'http://lsq.aksw.org/datasets/bench-drugbank-lsq2',
             'http://lsq.aksw.org/datasets/bench-genage-lsq2',
             'http://lsq.aksw.org/datasets/bench-gendr-lsq2',
             'http://lsq.aksw.org/datasets/bench-gene-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-goa-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-hgnc-lsq2', 
-            // 'http://lsq.aksw.org/datasets/bench-homologene-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-irefindex-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-kegg-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-linkedGeoData-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-linkedspl-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-mgi-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-ncbigene-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-omim-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-pharmgkb-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-sabiork-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-sgd-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-sidr-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-swdf-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-taxonomy-lsq2',
+            'http://lsq.aksw.org/datasets/bench-goa-lsq2',
+            'http://lsq.aksw.org/datasets/bench-hgnc-lsq2', 
+            'http://lsq.aksw.org/datasets/bench-homologene-lsq2',
+            'http://lsq.aksw.org/datasets/bench-irefindex-lsq2',
+            'http://lsq.aksw.org/datasets/bench-kegg-lsq2',
+            'http://lsq.aksw.org/datasets/bench-linkedGeoData-lsq2',
+            'http://lsq.aksw.org/datasets/bench-linkedspl-lsq2',
+            'http://lsq.aksw.org/datasets/bench-mgi-lsq2',
+            'http://lsq.aksw.org/datasets/bench-ncbigene-lsq2',
+            'http://lsq.aksw.org/datasets/bench-omim-lsq2',
+            'http://lsq.aksw.org/datasets/bench-pharmgkb-lsq2',
+            'http://lsq.aksw.org/datasets/bench-sabiork-lsq2',
+            'http://lsq.aksw.org/datasets/bench-sgd-lsq2',
+            'http://lsq.aksw.org/datasets/bench-sidr-lsq2',
+            'http://lsq.aksw.org/datasets/bench-swdf-lsq2',
+            'http://lsq.aksw.org/datasets/bench-taxonomy-lsq2',
             'http://lsq.aksw.org/datasets/bench-wikidata-interval1-organic-lsq2',
             'http://lsq.aksw.org/datasets/bench-wikidata-interval2-organic-lsq2',
             'http://lsq.aksw.org/datasets/bench-wikidata-interval3-organic-lsq2',
@@ -331,7 +429,7 @@ async function main() {
             'http://lsq.aksw.org/datasets/bench-wikidata-interval5-organic-lsq2',
             'http://lsq.aksw.org/datasets/bench-wikidata-interval6-organic-lsq2',
             'http://lsq.aksw.org/datasets/bench-wikidata-interval7-organic-lsq2',
-            // 'http://lsq.aksw.org/datasets/bench-wormbase-lsq2'
+            'http://lsq.aksw.org/datasets/bench-wormbase-lsq2'
         ]
     });
 }
